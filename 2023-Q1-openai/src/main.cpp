df189076af52bd810407bd5c0a7f280e298dc387
Siska <4705114+SiskaAdam@users.noreply.github.com>
2023-01-19 13:52:58 +0100

    #2 remove unneeded comments generated by XCode.

--- 2023-Q1-openai/src/main.cpp
+++ 2023-Q1-openai/src/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  OpenAIChallenge
-//
-//  Created by Siska Ádám on 2023. 01. 19..
-//
-
 #include <iostream>
 
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
